4e1ede842c26ea08024f42bd2e263c12afb263a7
ricardormeza <ricardo.rmeza@live.com>
2024-03-15 18:23:55 -0700

    ultimo ajuste

--- wp-content/themes/agenciaalkimiamenu/js/app.js
+++ wp-content/themes/agenciaalkimiamenu/js/app.js
@@ -3,11 +3,19 @@ function temaalkimia(){
     // alert('funciona');
     if(document.querySelector('.swiper')){
         const opciones = {
-
+            loop: true,
+            autoplay:{
+                delay:3000
+            }
         }
         new  Swiper('.swiper', opciones);
     }
 
-}
+
+    // MENU HAMBURGUESA
+    const hamburger = document.querySelector('.burguer-menu svg')
+    hamburger.addEventListener( 'click', function(){
+        
+    } )
 
 document.addEventListener('DOMContentLoaded', temaalkimia);
